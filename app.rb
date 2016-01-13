2c04b986ff3fca53b1aa0291c78f6c84b0690187
Marck <mercercountynj@gmail.com>
2016-01-13 14:48:32 -0500

    email working

--- app.rb
+++ app.rb
@@ -1,43 +1,10 @@
 #create "Gemfile" file, add "gem 'sinatra'", and run bundle install
 require 'sinatra'
 require 'sendgrid-ruby'
-#
-#name = "mark"
-#email="mercercountynj@gmail.com"
-#
-#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 
-#
-## As a hash
-#client = SendGrid::Client.new(api_user: ENV['SENDGRID_USERNAME'], api_key: ENV['SENDGRID_PASSWORD'])
-#
-## Or as a block
-#client = SendGrid::Client.new do |c|
-#  c.api_user = ENV['SENDGRID_USERNAME']
-#  c.api_key = ENV['SENDGRID_PASSWORD']
-#end
-#
-## or as a block with the API key only #
-#client = SendGrid::Client.new do |c|
-#  c.api_key = ENV['SENDGRID_API_KEY']
-#end
-#
-#
-#
-#
-#
-#mail = SendGrid::Mail.new do |m|
-#  m.to = 'jon@nycda.com'
-#  m.from = 'jon@nycda.com'
-#  m.subject = 'Hello world!'
-#  m.text = 'I heard you like pineapple.'
-#end
-#
-#res = client.send(mail)
-#puts res.code
-#puts res.body
 
 
-get '/home' do
+
+get '/' do
     erb :home
 end
 
@@ -54,7 +21,30 @@ get '/bar' do
 end
 
 get '/contact' do
+    erb :contact 
+end
+
+
+
+post '/email-request' do
+    client = SendGrid::Client.new do |c|
+    c.api_key = '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'
+end
+
+    mail = SendGrid::Mail.new do |m|
+    m.to = 'supraracingperformance@yahoo.com'
+    m.from = 'mer@nycda.com'
+    m.subject = 'Hello world!'
+    m.text = 'I heard you like pineapple.'
+end
+
+    res = client.send(mail)
+    puts res.code
+    puts res.body
+    
+
     erb :contact
+    
 end
 
 get '/dining' do
